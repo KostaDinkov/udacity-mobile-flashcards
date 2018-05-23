215759ac474bb0b8019e7f913318b072f1d0d110
Kosta <k.v.dinkov@gmail.com>
2018-05-23 10:31:15 +0300

    add: redux; react-redux; sample data;

--- App.js
+++ App.js
@@ -4,17 +4,23 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
 import DeckList from './components/DeckList'
+import {importData} from './actions/index';
+import {fetchAllData } from './api/data'
+
+
+const store = createStore(reducer);
+store.dispatch(importData(fetchAllData()));
 
 export default class App extends React.Component {
 
 
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={store}>
         <View style={styles.container}>
 
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Mobile Flashcards App</Text>
+
           <DeckList/>
         </View>
       </Provider>
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import {View,Text} from 'react-native';
 import {connect} from 'react-redux'
-import {importData} from '../actions/index';
-import {fetchAllData } from '../api/data'
+
 
 class DeckList extends React.Component{
 
-  componentWillMount(){
-
-    let data = fetchAllData();
-    this.props.dispatch(importData(data));
-  }
 
   render(){
+    debugger;
+
+    const {decks} = this.props.store;
     return (
       <View>
-        <Text>{JSON.stringify(this.props.store)}</Text>
+        <Text>Available Decks:</Text>
+        {Object.keys(decks).map(id=>(
+          <View key = {id}>
+            <Text>{decks[id].name}</Text>
+            <Text>Questions:{decks[id].cards.length}</Text>
+          </View>
+        ))}
       </View>
     )
   }
